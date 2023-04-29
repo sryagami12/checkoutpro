7547849ac8ce03c8df06d4b38e7379afb406c10e
DeyvyMY <odr5_deivioscar@hotmail.com>
2023-04-29 17:19:26 -0500

    make it better

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 class PostController extends Controller
 {
 
-    public function store(Request $request)
+    public function postStore(Request $request)
     {
         //
     }
