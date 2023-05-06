8065252faa37b9fb293a421e36e16e300a857117
DeyvyMY <odr5_deivioscar@hotmail.com>
2023-05-06 08:56:02 -0500

    google sheets

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -10,7 +10,7 @@ class PostController extends Controller
 
     public function store(Request $request)
     {
-        $values = Sheets::spreadsheet('1mqsbIrMoEH6SQ0V5PaQaQxLhpRkSrAJt_2VNRVkltiw')->sheet('sales')->all();
+        $values = Sheets::spreadsheet('1mqsbIrMoEH6SQ0V5PaQaQxLhpRkSrAJt_2VNRVkltiw')->sheet('sales')->get();
         return $values;
     }
 }
