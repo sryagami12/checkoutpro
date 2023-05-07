fb320ba73006511d5c263a1f6462fbccc645c5d7
DeyvyMY <odr5_deivioscar@hotmail.com>
2023-05-07 17:16:48 -0500

    show link product

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -33,7 +33,7 @@
                                 <td>
                                     <div class="dropdown">
                                         <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                          Dropdown button
+                                          Options
                                         </button>
                                         <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
                                           <a class="dropdown-item" href="#">Open Link</a>
