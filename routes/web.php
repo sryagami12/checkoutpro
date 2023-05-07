452068fd0fa0bd7e388834a50f16b6ba22ee9292
DeyvyMY <odr5_deivioscar@hotmail.com>
2023-05-07 14:02:03 -0500

    holi

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,7 @@ Route::post('store', [PostController::class, 'store']);
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
