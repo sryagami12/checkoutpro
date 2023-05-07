9540452ef6ec6128b50c461f8d62f52710f56f47
DeyvyMY <odr5_deivioscar@hotmail.com>
2023-05-06 19:53:58 -0500

    google sheets

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,6 @@ Route::get('/', function () {
 });
 
 Route::post('store', [PostController::class, 'store']);
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
