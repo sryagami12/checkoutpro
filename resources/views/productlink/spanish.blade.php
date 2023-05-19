42de6b78762593a03d1562fe99ac339c72a53cc2
DeyvyMY <odr5_deivioscar@hotmail.com>
2023-05-19 18:03:56 -0500

    country autoselect

--- resources/views/productlink/spanish.blade.php
+++ resources/views/productlink/spanish.blade.php
@@ -719,7 +719,7 @@
                                     console.log("City:", userCity);
                                     console.log("Country:", userCountry);
                                     const $select = document.querySelector('#checkout_shipping_country_code');
-                                    $select.textContent = userCountry;        
+                                    $select.innerText = userCountry;        
                                  });
                                  
                               </script>
