317b742d516c623d2e4302b450b04c6a3790e72d
DeyvyMY <odr5_deivioscar@hotmail.com>
2023-05-21 15:29:58 -0500

    dropbox

--- app/Http/Controllers/ProductLinkController.php
+++ app/Http/Controllers/ProductLinkController.php
@@ -45,7 +45,6 @@ class ProductLinkController extends Controller
 
             $link = $client->createSharedLinkWithSettings($path);
 
-            return $link['url'];
 
             //$image = $request->file('product_image_path');
             //$filename = time() . '.' . $image->getClientOriginalExtension();
@@ -57,7 +56,7 @@ class ProductLinkController extends Controller
                 'product_name'                  => $request->input('product_name'),
                 'product_price'                 => $request->input('product_price'),
                 'product_quantity'              => $request->input('product_quantity'),
-                'product_image_path'            => "Holi",
+                'product_image_path'            => $link['url'],
                 'checkout_free_option_label'    => $request->input('checkout_free_option_label'),
                 'checkout_free_option_Value'    => $request->input('checkout_free_option_Value'),
                 'checkout_express_option_label' => $request->input('checkout_express_option_label'),
