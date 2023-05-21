c4ce93496cb02b54963c385b897a3d27f459cfd0
DeyvyMY <odr5_deivioscar@hotmail.com>
2023-05-21 17:09:34 -0500

    hola

--- app/Http/Controllers/ProductLinkController.php
+++ app/Http/Controllers/ProductLinkController.php
@@ -48,6 +48,9 @@ class ProductLinkController extends Controller
                 ["requested_visibility" => "public"]
             );
 
+            $url = trim($link['url'], "?dl=0");
+            $url = $url."?raw=1";
+
             //$image = $request->file('product_image_path');
             //$filename = time() . '.' . $image->getClientOriginalExtension();
 
@@ -58,7 +61,7 @@ class ProductLinkController extends Controller
                 'product_name'                  => $request->input('product_name'),
                 'product_price'                 => $request->input('product_price'),
                 'product_quantity'              => $request->input('product_quantity'),
-                'product_image_path'            => $link['url'],
+                'product_image_path'            => $url,
                 'checkout_free_option_label'    => $request->input('checkout_free_option_label'),
                 'checkout_free_option_Value'    => $request->input('checkout_free_option_Value'),
                 'checkout_express_option_label' => $request->input('checkout_express_option_label'),
