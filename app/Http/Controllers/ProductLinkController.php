4e8cd70f607a146d179f1e9835423ac15374d458
DeyvyMY <odr5_deivioscar@hotmail.com>
2023-05-21 15:13:23 -0500

    dropbox

--- app/Http/Controllers/ProductLinkController.php
+++ app/Http/Controllers/ProductLinkController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 use DB;
+use App\Models\ProductLinkModel;
 
 class ProductLinkController extends Controller
 {
