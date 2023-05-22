6238a13583f846509fc439d6e30487cf1ac83d87
DeyvyMY <odr5_deivioscar@hotmail.com>
2023-05-22 16:13:35 -0500

    googe sheets

--- resources/views/scripts/currency.blade.php
+++ resources/views/scripts/currency.blade.php
@@ -518,7 +518,7 @@
                 document.getElementById('taxes_dolar').value = taxes.toFixed(2);
                 document.getElementById('total_dolar').value = total.toFixed(2);
                 document.getElementById('client_currency').value = currency;
-                document.getElementById('client_currency_exchange').value = changevalue;
+                document.getElementById('client_currency_exchange').value = parseFloat(changevalue).toFixed(2);
 
                 productprice        = Math.round(productprice*changevalue);
                 taxes               = Math.round(taxes*changevalue);
